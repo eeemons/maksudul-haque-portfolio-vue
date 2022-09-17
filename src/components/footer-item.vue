baf4bada175bda699d19341cd2ee317f204bafa4
Maksudul Haque <e01716@gmail.com>
2022-09-17 17:40:51 +0600

    Added footer

--- src/components/footer-item.vue
+++ src/components/footer-item.vue
@@ -2,17 +2,17 @@
 
 <template>
   <div class="container">
-    <div class="column">
+    <div class="column column-one">
       <p>Socials</p>
       <p>Copyright Maksudul Haque</p>
       <p>Designed By Maksudul Haque</p>
     </div>
-    <div class="column">
+    <div class="column column-two">
       <p>Contacts</p>
       <p>01684683581</p>
       <p>eeemons@live.com</p>
     </div>
-    <div class="column">
+    <div class="column column-three">
       <p>UI/UX Designer</p>
       <p>Front-end Developer</p>
     </div>
@@ -41,5 +41,37 @@ p:hover {
   justify-content: space-around;
   align-items: center;
   overflow-x: hidden;
+}
+
+@media screen and (max-width: 425px) {
+  h1,
+  p,
+  h2,
+  h3,
+  h4 {
+    color: #707070;
+    font-size: 1rem;
+  }
+  .container {
+    flex-direction: column;
+    position: relative;
+  }
+  .column-one {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+  }
+
+  .column-two {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
+  .column-three {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+  }
 }
 </style>
